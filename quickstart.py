62fc06955195a88f392a4de2029686dc4f0ec773
nobody-night <king@xiaoyy.org>
2019-09-01 14:56:13 +0800

    update quickstart

--- quickstart.py
+++ quickstart.py
@@ -3,28 +3,61 @@
 import time
 
 from stopwatch import Stopwatch
-from stopwatch import StatusError
+from stopwatch import StopwatchStatus
 
 
 # define main function
 
-def main() -> int:
-    test_stopwatch: Stopwatch = Stopwatch(6)
-    test_stopwatch.start()
+def main():
+
+    # instantiate stopwatch
+
+    demo_stopwatch: Stopwatch = Stopwatch(
+        default_precision = 3   # ignorable
+    )
+
+    # make stopwatch start timing
+    
+    demo_stopwatch.start()
+
+    # the first stage of the simulation program
 
     time.sleep(3)
 
-    test_stopwatch.lap('test1')
+    # it takes time to record the first phase of the program
+
+    demo_stopwatch.lap('stage1')
+
+    # the second stage of the simulation program
 
     time.sleep(1)
 
-    test_stopwatch.lap('test2')
-    
-    print('Total: ' + str(test_stopwatch.stop()))
-    print('Test1: ' + str(test_stopwatch.get_lap('test1')))
-    print('Test2: ' + str(test_stopwatch.get_lap('test2')))
+    # it takes time to record the second phase of the program
 
-    return 0
+    demo_stopwatch.lap('stage2')
+    
+    # get the status of stopwatch
+
+    if demo_stopwatch.get_status() == StopwatchStatus.Started:
+        print('stopwatch is timing...')
+
+    # stop and print the timing of the stopwatch
+    
+    print(
+        (
+            'total: {TOTAL_SECONDS} seconds\n'
+            'stage1: {STAGE1_SECONDS} seconds\n'
+            'stage2: {STAGE2_SECONDS} seconds'
+        ).format(
+            TOTAL_SECONDS = demo_stopwatch.stop(), 
+            STAGE1_SECONDS = demo_stopwatch.get_lap('stage1'), 
+            STAGE2_SECONDS = demo_stopwatch.get_lap('stage2')
+        )
+    )
+
+    # reset stopwatch
+
+    demo_stopwatch.reset()
 
 
 # define virtual main function
